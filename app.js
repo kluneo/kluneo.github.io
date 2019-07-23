a99861d3a9edaea95f43343b712c06c625756170
Valentin <valentin@iMac-de-Valentin.local>
2019-07-23 16:27:35 +0200

    h

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 
-TweenMax.to(".premium", 1, {opacity:1, delay:.15, ease: Ease.easeInOut});
+      const btn = document.querySelector(".btn");
+      const box = document.querySelector(".overlay");
 
+// const showBox = new TimelineMax();
+// //alias for brevity
+// const sb = showBox;
 
+// const hb = new TimelineMax(); //hidebox
 
-
-
+btn.addEventListener("click", function() {
+  if (!box.classList.contains("active")) {    
+    TweenMax.fromTo(box, 0.5, { opacity: 0, y: 0 }, { opacity: 1, y: 1 });
+    box.classList.add("active");
+  } else {
+    TweenMax.fromTo(box, 0.5, { opacity: 1, y: 0 }, { opacity: 0, y: -100 });
+    box.classList.remove("active");
+  }
+}); 
